33eff0c1bd3024d6a47e64b81be9245799448532
Suhjin Kang <55722299+skang9810@users.noreply.github.com>
2024-11-20 10:07:26 +0900

    backend url fix (#8)
    * backend url fix
    
    * netlify.toml
    
    * netlify.toml fix
    
    * back to http

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,17 @@
 import { useEffect } from 'react';
 
 const HomePage = () => {
+  const API_URL =
+    import.meta.env.MODE === 'production' ? 'http://3.36.131.224/api' : '/api';
+
   useEffect(() => {
     const testApi = async () => {
       try {
-        const response = await fetch(`/api/test`);
+        const response = await fetch(`${API_URL}/test`);
         const data = await response.json();
         console.log('API Response:', data);
       } catch (error) {
-        console.error('Error connecting to API:', error);
+        console.error(error);
       }
     };
 
