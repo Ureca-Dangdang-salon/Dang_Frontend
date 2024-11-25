f1243f7327291a1af3775d613f63681c3719a107
Jeongmin <srm0609@naver.com>
2024-11-25 23:49:02 +0900

    Create: Templates

--- src/pages/NewRequest.jsx
+++ src/pages/NewRequest.jsx
@@ -1,19 +1,31 @@
-import { SurveyHeader } from '@components/Common/SurveyHeader';
-import NewRequest from '@components/NewRequest/templates';
-import { styled } from '@mui/material';
-
-const Layout = styled('div')(() => ({
-  width: '100%',
-  height: 'calc(100% - 80px)',
-  marginBottom: '120px',
-}));
+import { Box } from '@mui/material';
+import { SurveyHeader } from '@components/Common/SurveyHeader/SurveyHeader';
+import NewRequest from '@components/NewRequest/templates/NewRequest';
+import usePageStore from '@/store/usePageStore';
 
 const NewRequestPage = () => {
+  const { newRequestStep, setNewRequestStep, dogStep, setDogStep } =
+    usePageStore();
+
+  const PrevStep = () => {
+    if (dogStep) {
+      dogStep === 1 ? setDogStep(0) : setDogStep(dogStep - 1);
+    } else setNewRequestStep(newRequestStep - 1);
+  };
+
   return (
-    <Layout>
-      <SurveyHeader totalPage={2} currPage={1} label="견적 요청" />
-      <NewRequest />
-    </Layout>
+    <Box>
+      <SurveyHeader
+        totalPage={2}
+        currPage={dogStep ? dogStep : newRequestStep}
+        label="견적 요청"
+        delBack={newRequestStep === 1}
+        backHandler={PrevStep}
+      />
+      <Box p={4} color="text.main">
+        <NewRequest />
+      </Box>
+    </Box>
   );
 };
 
