ec7386c4fe24d9f487e273eb01ab19f15e947e16
Jeongmin <srm0609@naver.com>
2024-11-28 15:24:00 +0900

    Fix: onclick

--- src/components/Chatting/atoms/MyRequestItem.jsx
+++ src/components/Chatting/atoms/MyRequestItem.jsx
@@ -1,4 +1,5 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Modal } from '@components/Common/Modal/Modal';
+import { Box, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const MyRequestItem = ({ deadline }) => {
@@ -16,6 +17,7 @@ const MyRequestItem = ({ deadline }) => {
         gap: 2,
         boxShadow: '0px 1px 5px 0px rgba(51, 51, 51, 0.08)',
         borderRadius: '10px',
+        cursor: 'pointer',
       }}
       onClick={() => navigation('1')}
     >
@@ -25,26 +27,24 @@ const MyRequestItem = ({ deadline }) => {
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'center',
-          cursor: 'pointer',
         }}
       >
         <Typography>댕댕1, 흰둥1 견적</Typography>
         <Typography>2024년 11월 16일</Typography>
       </Box>
-      {deadline ? (
-        <Typography variant="body2">마감</Typography>
-      ) : (
-        <Button
-          color="delete"
-          sx={{
-            paddingX: 1,
-            borderRadius: '10px',
-            fontSize: '14px',
-          }}
-        >
-          견적 그만 받기
-        </Button>
-      )}
+      <Box onClick={(e) => e.stopPropagation()}>
+        {deadline ? (
+          <Typography variant="body2">마감</Typography>
+        ) : (
+          <Modal
+            buttonColor="delete"
+            openLabel="견적 그만 받기"
+            leftLabel="취소"
+            rightLabel="그만 받기"
+            title="견적을 그만 받으시겠습니까?"
+          />
+        )}
+      </Box>
     </Box>
   );
 };
