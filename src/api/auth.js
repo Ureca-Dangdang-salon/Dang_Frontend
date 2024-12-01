9419314f876d9f1ec0557684479339efd42981e8
Jeongmin <srm0609@naver.com>
2024-12-01 17:51:00 +0900

    feat: privateRoute 설정

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,11 +3,23 @@ import { AuthController } from './requestUrls';
 
 export const join = async (role, district_id) => {
   try {
-    await apiClient.post(AuthController.join, {
+    const { data } = await apiClient.post(AuthController.join, {
       role: role,
       districtId: district_id,
     });
-    return true;
+    if (data.response === '회원가입에 성공했습니다.') return true;
+    else return false;
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+};
+
+export const loginCheck = async () => {
+  try {
+    const { data } = await apiClient.get(AuthController.checkLogin);
+    if (data.response === '로그인이 되어있지 않습니다.') return false;
+    return !!data.response?.login;
   } catch (e) {
     console.log(e);
     return false;
