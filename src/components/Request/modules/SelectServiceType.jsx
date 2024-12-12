97e9a00fe13a1c3d1614f553613b3bb59521034a
Jeongmin <srm0609@naver.com>
2024-12-12 09:49:32 +0900

    Fix: 견적요청(유저) 폼 초기화

--- src/components/Request/modules/SelectServiceType.jsx
+++ src/components/Request/modules/SelectServiceType.jsx
@@ -4,13 +4,13 @@ import SubTitle from '../atoms/SubTitle';
 import useRequestStore from '@/store/useRequestStore';
 
 const SelectServiceType = () => {
-  const { serviceTypes, toggleServiceType } = useRequestStore();
+  const { serviceType, toggleServiceType } = useRequestStore();
 
   return (
     <div>
       <SubTitle title="서비스 진행 형태" />
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-        {serviceTypes.map((e, idx) => (
+        {serviceType.map((e, idx) => (
           <RadioButton
             key={idx}
             label={e.key}
