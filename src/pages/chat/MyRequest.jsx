ba6798b81ed89253ffd942865a86eb8534a09008
Jeongmin <srm0609@naver.com>
2024-12-13 11:47:55 +0900

    Feat: 내 견적요청보기

--- src/pages/chat/MyRequest.jsx
+++ src/pages/chat/MyRequest.jsx
@@ -1,15 +1,31 @@
 import { Box } from '@mui/material';
 import MyRequestItem from '@components/Chatting/atoms/MyRequestItem';
 import { DetailHeader } from '@components/Common/DetailHeader/DetailHeader';
+import { useState, useEffect } from 'react';
+import { getRequestMy } from '@/api/chat';
+import EmptyContent from '@components/Layout/EmptyContent';
 
 const MyRequest = () => {
+  const [list, setList] = useState();
+
+  useEffect(() => {
+    const fetchList = async () => {
+      const res = await getRequestMy();
+      setList(res);
+    };
+    fetchList();
+  }, []);
+
   return (
     <Box>
       <DetailHeader label="내 견적 요청" />
       <Box p={4}>
         <Box display="flex" flexDirection="column" alignItems="center">
-          <MyRequestItem deadline={true} />
-          <MyRequestItem />
+          {list?.length > 0 ? (
+            list.map((e, idx) => <MyRequestItem key={idx} data={e} />)
+          ) : (
+            <EmptyContent title="요청 내역이 없습니다." />
+          )}
         </Box>
       </Box>
     </Box>
--- src/pages/chat/MyRequestDetail.jsx
+++ src/pages/chat/MyRequestDetail.jsx
@@ -1,14 +1,29 @@
 import { Box } from '@mui/material';
 import { DetailHeader } from '@components/Common/DetailHeader/DetailHeader';
 import DetailItem from '@components/Chatting/atoms/DetailItem';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getRequestMyDetail } from '@/api/chat';
 
 const MyRequestDetail = () => {
+  const { id } = useParams();
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getRequestMyDetail(id);
+      setData(res);
+    };
+    fetchData();
+  }, []);
+
   return (
     <Box>
       <DetailHeader label="댕댕1, 횐둥1 견적" />
       <Box p={4}>
         <Box display="flex" flexDirection="column" alignItems="center">
-          <DetailItem />
+          {data?.length > 0 &&
+            data.map((e, idx) => <DetailItem key={idx} data={e} />)}
         </Box>
       </Box>
     </Box>
