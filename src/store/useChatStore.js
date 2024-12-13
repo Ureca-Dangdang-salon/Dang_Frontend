8252eaf3de412cd284b972c47b0c95170e46cc41
Jeongmin <srm0609@naver.com>
2024-12-13 17:24:00 +0900

    Delete: 미사용 코드

--- src/store/useChatStore.js
+++ src/store/useChatStore.js
@@ -1,11 +1,6 @@
 import { create } from 'zustand';
 
 const useChatStore = create((set) => ({
-  customerProfile: null,
-  setCustomerProfile: (data) => set({ customerProfile: data }),
-  groomerProfile: null,
-  setGroomerProfile: (data) => set({ groomerProfile: data }),
-
   otherProfile: null,
   setOtherProfile: (data) => set({ otherProfile: data }),
 
