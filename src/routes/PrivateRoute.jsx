3b47f8a0af18d3c7c8efc25f9a3b5f321340a648
skang9810 <suhjin9810@gmail.com>
2024-12-17 00:05:21 +0900

    private route fix

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,7 +4,7 @@ import { loginCheck } from '@/api/auth';
 import useUserStore from '@/store/useUserStore';
 import { handleEnableNotifications } from '@/firebase/firebaseMessaging';
 import paths from './paths';
-import Loading from '@components/Layout/Loading';
+import { Typography } from '@mui/material';
 
 const PrivateRoute = () => {
   const { setRole, loggedIn, setLoggedIn, setNotificationEnabled, setUserId } =
@@ -20,23 +20,24 @@ const PrivateRoute = () => {
       setNotificationEnabled(res.notificationEnabled);
       setLoading(false);
 
-        const notificationOn = localStorage.getItem('notificationOn');
+      const notificationOn = localStorage.getItem('notificationOn');
 
-        if (res.login && notificationOn !== 'true') {
-          await handleEnableNotifications();
-          localStorage.setItem('notificationOn', 'true');
-        }
-      } catch (error) {
-        console.error('로그인 체크에 실패했습니다:', error);
-        setLoggedIn(false);
-        setLoading(false);
+      if (res.login && notificationOn !== 'true') {
+        await handleEnableNotifications();
+        localStorage.setItem('notificationOn', 'true');
       }
-    };
+    } catch (error) {
+      console.error('로그인 체크에 실패했습니다:', error);
+      setLoggedIn(false);
+      setLoading(false);
+    }
+  };
 
+  useEffect(() => {
     checkLogin();
   }, []);
 
-  if (loading) return <Loading />;
+  if (loading) return <Typography>Loading...</Typography>;
 
   return loggedIn ? <Outlet /> : <Navigate to={paths.login} />;
 };
